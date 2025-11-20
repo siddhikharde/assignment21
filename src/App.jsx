456a3fd88efbe4a6af8d88f24574b1b94d836d3b
siddhikharde <siddhikharde2005@gmail.com>
2025-11-20 23:27:14 +0530

    toast added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import React, { use, useEffect, useState } from 'react'
+import { toast } from 'react-hot-toast';
 import "./App.css"
 function FormHandling() {
   const [name, setName] = useState("");
   const [age, setAge] = useState("");
   const [error, setError] = useState("");
   const [city, setCity] = useState("");
-  const saveName = () => {
+  const [gender, setGender] = useState("");
+  const saveInfo = () => {
     if (name.length > 2 && name.length < 20) {
       localStorage.setItem("name", name);
     
     }
    localStorage.setItem("age", age);
    localStorage.setItem("city", city);
+   localStorage.setItem("gender", gender);
+  toast.success('Successfully Saved!');
 
   }
   useEffect(() => {
@@ -98,21 +102,33 @@ function FormHandling() {
 
         <div className='radio-btn'>
           <div><span>Male </span>
-          <input type="radio" name="gender" value="male" /></div>
+          <input type="radio" name="gender" value="male" onChange={(e)=>{
+            if(e.target.checked){
+              setGender(e.target.value)
+            }
+
+          }}
+          checked={gender === "male"} /></div>
           <div>
             <span>
             Female  </span>
-          <input type="radio" name="gender" value="Female" />
+          <input type="radio" name="gender" value="Female" onChange={(e)=>{
+            if(e.target.checked){
+              setGender(e.target.value);
+            }
+          }} 
+          checked={gender === "Female"} />
           </div></div>
 
         <div className='btn-con'>
-          <button className={`btn ${name.length < 2 ? "btnDisable" : null}`} onClick={saveName}>Save</button>
+          <button className={`btn ${name.length < 2 ? "btnDisable" : null}`} onClick={saveInfo}>Save</button>
           <button className='btn' onClick={() => {
             setName("")
             setAge("")
             localStorage.setItem("name", "")
             localStorage.setItem("age", "")
             localStorage.setItem("city", "")
+            localStorage.setItem("gender", "")
           }}>Clear</button>
         </div>
       </div>
